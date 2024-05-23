7f36f788ce97190b6745a7c5a3b92021f6b9ec31
styhrd <juanmiguelcaparas76@gmail.com>
2024-05-23 09:36:06 +0800

    Mongo DB Setup

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 //imports
 import express from 'express';
-
+import dotenv from 'dotenv'
+import connectDB from './config/db.js';
+import testRoutes from './routes/testRoutes.js'
+import cors from 'cors'
+import morgan from 'morgan';
 
 const app = express()
-
+dotenv.config()
+connectDB()
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
 
 //routes
 
-app.get('/', (req, res) => {
-    res.send("<h1>Welcome</h1>")
-})
+app.use('/api/v1/test', testRoutes)
 
-
+const PORT = process.env.PORT || 8080
 //listen
 app.listen(8080, () => {
-    console.log("Server Is Running sir")
+    console.log(`Server Is Running sir on ${PORT}`)
 })
